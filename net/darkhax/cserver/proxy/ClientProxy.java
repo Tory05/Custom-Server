30b5f8b06516742707acc5da121e68faa9f3d7db
Tyler Hancock <darklime@live.ca>
2013-11-29 20:55:42 -0700

    Created code to add the server.

--- net/darkhax/cserver/proxy/ClientProxy.java
+++ net/darkhax/cserver/proxy/ClientProxy.java
@@ -1,5 +1,34 @@
 package net.darkhax.cserver.proxy;
 
-public class ClientProxy extends ServerProxy {
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.multiplayer.ServerData;
+import net.minecraft.client.multiplayer.ServerList;
 
+public class ClientProxy extends ServerProxy {
+	
+	public static void setServer(String name, String address) {
+		
+		boolean serverExists = false;
+		
+		ServerList list = new ServerList(Minecraft.getMinecraft());
+        list.loadServerList();
+         
+        for (int i = 0; i < list.countServers(); i++) {
+        	
+        	ServerData servers = list.getServerData(i);
+        	
+        	if (servers.serverName.equals(name) && servers.serverIP.equals(address)) {
+        		
+        		serverExists = true;
+        		break;
+        	}
+        }
+         
+        if (!serverExists) {
+        	
+                 list.addServerData(new ServerData(name, address));
+        }
+         
+        list.saveServerList(); 
+	}
 }
