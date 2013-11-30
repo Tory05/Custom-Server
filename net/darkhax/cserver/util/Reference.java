a98134f5b0c61bed003d749e3b4f9fcf81ea8572
Tyler Hancock <darklime@live.ca>
2013-11-29 19:55:05 -0700

    Fixed proxy names.

--- net/darkhax/cserver/util/Reference.java
+++ net/darkhax/cserver/util/Reference.java
@@ -5,6 +5,6 @@ public class Reference {
 	public static final String NAME = "Custom Server";
 	public static final String VERSION = "1.0.0";
 	public static final String ID = "CustomServer";
-	public static final String SERVER = "net.darkhax.custom.proxy.ServerProxy";
-	public static final String CLIENT = "net.darkhax.custom.proxy.ClientProxy";
+	public static final String SERVER = "net.darkhax.cserver.proxy.ServerProxy";
+	public static final String CLIENT = "net.darkhax.cserver.proxy.ClientProxy";
 }
