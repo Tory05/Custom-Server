901b62bc207f1b14e140b8dddd40144027d1f70b
Tyler Hancock <darklime@live.ca>
2013-11-29 19:48:59 -0700

    Set up the reference class.

--- net/darkhax/cserver/util/Reference.java
+++ net/darkhax/cserver/util/Reference.java
@@ -2,4 +2,9 @@ package net.darkhax.cserver.util;
 
 public class Reference {
 
+	public static final String NAME = "Custom Server";
+	public static final String VERSION = "1.0.0";
+	public static final String ID = "CustomServer";
+	public static final String SERVER = "net.darkhax.custom.proxy.ServerProxy";
+	public static final String CLIENT = "net.darkhax.custom.proxy.ClientProxy";
 }
